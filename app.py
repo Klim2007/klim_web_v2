6270838376b139007d65caddbc010ad8f2ab3865
Klim2007 <104355761+Klim2007@users.noreply.github.com>
2023-07-06 16:46:25 +0000

    dynamic pages

--- app.py
+++ app.py
@@ -1,25 +1,29 @@
 from flask import Flask, render_template, jsonify
 from database import load_jobs_db
+from database import load_id_db
 
-
-app = Flask(__name__)
-
-
+app = Flask(__name__, static_folder='static')
 
 @app.route("/")  #part of domain name
 def hell():
   return render_template('home.html', jobs=load_jobs_db())
 
-
-@app.route("/api/JOBS")
+@app.route("/api/jobs")
 def list_jobs():
   jobs = load_jobs_db()
   return jsonify(jobs)
 
+@app.route("/job/<int:id>")
+def show_job(id):
+  job = load_id_db(id)
+  if job is None:
+    return jsonify({'error': 'Job not found'})
+  else:
+    return render_template('jobpage.html', job=job)
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0',port=5001 ,debug=True)
+  app.run(host='0.0.0.0', port=5000,  debug=True)
 
 #Start the app on port 5001
 #if: __name__ == '__main__':
-    #app.run(port=5001) 
+#app.run(port=5001)
--- database.py
+++ database.py
@@ -2,14 +2,14 @@ import sqlalchemy
 from sqlalchemy import text, create_engine
 import pymysql
 import os
-
+# Cnnection to DB with secret con details
 con_data = os.environ['DB_connection_details']
-
 
 engine = create_engine(con_data, connect_args={"ssl": {
   "ca": "cacert.pem",
 }})
 
+# List of jobs on front page
 def load_jobs_db():
   with engine.connect() as conn:
     result = conn.execute(text("SELECT * FROM jobs"))
@@ -17,15 +17,19 @@ def load_jobs_db():
   for row in result.all():
     jobs.append(row._asdict())
   return jobs
-"""
-with engine.connect() as conn:
-  result = conn.execute(text("SELECT * FROM jobs"))
 
 
-fetch = []
-for row in result.all():
-  fetch.append(row._asdict())
-print(fetch)
-
-"""
-
+def load_id_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT * FROM jobs WHERE id = :val"),
+                          {"val": id})
+  row = result.fetchone()
+  if row is None:
+    return None
+  else:
+    column_names = result.keys(
+    )  # Retrieve column names from the result object
+    row_values = row  # Row values don't need to be retrieved separately
+    data = dict(zip(column_names, row_values))
+   
+    return data
